ca8976e7127f5c176fd63e79af19bcacaf8fa4b8
Andrei Baskakau <Andrei_Baskakau@epam.com>
2015-09-30 16:59:12 +0300

    12

--- Test.groovy
+++ Test.groovy
@@ -3,8 +3,8 @@
  * Created by abaskakau on 9/30/15.
  */
 
-def script = new GroovyScriptEngine('.').with {
-    loadScriptByName('Library')
+def script = new GroovyScriptEngine().with {
+    loadScriptByName('Main.groovy')
 }
 
 out("asfd")
