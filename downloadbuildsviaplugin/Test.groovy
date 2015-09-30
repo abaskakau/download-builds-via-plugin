3ad6519064f185bbd8b6cf492967d606ffee6b52
Andrei Baskakau <Andrei_Baskakau@epam.com>
2015-09-30 16:40:11 +0300

    3

--- downloadbuildsviaplugin/Test.groovy
+++ downloadbuildsviaplugin/Test.groovy
@@ -4,6 +4,6 @@ package downloadbuildsviaplugin
  * Created by abaskakau on 9/30/15.
  */
 
-evaluate(new File("./lib/Library.groovy"))
+evaluate(new File("./downloadbuildsviaplugin/lib/Library.groovy"))
 
 out("test")
