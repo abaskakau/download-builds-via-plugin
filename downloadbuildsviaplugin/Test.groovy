824bd48bf9aeabfccfb7a788a622cbe8b8217e34
Andrei Baskakau <Andrei_Baskakau@epam.com>
2015-09-30 16:44:47 +0300

    6

--- downloadbuildsviaplugin/Test.groovy
+++ downloadbuildsviaplugin/Test.groovy
@@ -1,9 +1,11 @@
 package downloadbuildsviaplugin
 
+import downloadbuildsviaplugin.lib.Library
+
 /**
  * Created by abaskakau on 9/30/15.
  */
 
 evaluate(new File(this.binding.build.project.workspace.toString()+"/downloadbuildsviaplugin/lib/Library.groovy"))
 
-out("test")
+Library.out("test")
